8f4e7e678d10e575e5b7a94bbcd78460c2ed6c98
kashifqazi <kmq2@njit.edu>
2019-08-02 19:56:31 -0400

    Adding atop to shell script

--- workload.sh
+++ workload.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 input="Host4"
 count=0
+atopsar -c 5 1500 > logs &
 while IFS= read -r line
 do
   result=$(bc -l <<<"${line}*100")
   echo "$result"
 
   count=`expr $count + 1`
-  if [ $count -ge 800 ]
+  if [ $count -ge 10 ]
   then
     break
   fi
   eval "sudo cpulimit -i -l $result stress-ng --memcpy 1 -t 10 --times --metrics-brief --perf --log-file stats"
-
+  cat stats >> statscomplete
 done < "$input"
 
