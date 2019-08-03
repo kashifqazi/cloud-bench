e8f2940557a418f1bb216bb76d21ee2031297b54
kashifqazi <kmq2@njit.edu>
2019-08-02 22:13:07 -0400

    Adding Second Chart to info.php

--- info.php
+++ info.php
@@ -1,13 +1,29 @@
 <?php
-$file = fopen("/home/ubuntu/Benchmarking/cloud-bench/Host4","r");
+$file = fopen("/home/ubuntu/Benchmarking/cloud-bench/cleanlogs","r");
+$file2 = fopen("/home/ubuntu/Benchmarking/cloud-bench/Host4","r");
 
 $dataPoints = [];
 while(! feof($file))
   {
-  array_push($dataPoints, array("y" => floatval(fgets($file))*100, "label" => "123"));
+  array_push($dataPoints, array("y" => floatval(fgets($file)), "label" => "123"));
   }
 
 fclose($file);
+
+$dataPoints2 = [];
+while(! feof($file2))
+  {
+  array_push($dataPoints2, array("y" => floatval(fgets($file2))*100, "label" => "123"));
+  }
+
+fclose($file2);
+
+$counter = count($dataPoints2) - count($dataPoints);
+echo $counter;
+for ($i = 1; $i <= $counter; $i++) {
+    array_push($dataPoints, array("y" => 0, "label" => "123"));
+}
+
 ?>
 
 <!DOCTYPE HTML>
@@ -17,24 +33,43 @@ fclose($file);
 window.onload = function () {
  
 var chart = new CanvasJS.Chart("chartContainer", {
+	title: {
+		text: "Actual Historical Load"
+	},
+	axisY: {
+		title: "Load"
+	},
+	data: [{
+		type: "line",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+
+
+chart.render();
+
+var chart2 = new CanvasJS.Chart("chartContainer2", {
         title: {
-                text: "Overall Expected Load"
+                text: "Expected Overall Load"
         },
         axisY: {
                 title: "Load"
         },
         data: [{
                 type: "line",
-                dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+                dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
         }]
 });
-chart.render();
- 
+
+
+chart2.render()
+
 }
 </script>
 </head>
 <body>
 <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<div id="chartContainer2" style="height: 370px; width: 100%;"></div>
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 </body>
 </html>   
