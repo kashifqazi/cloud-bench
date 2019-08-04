c1439bc2f2bc37fde1932d6d76e962f4cb601005
kashifqazi <kmq2@njit.edu>
2019-08-04 15:43:23 -0400

    Update installer with stress-ng installation

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 sudo apt update
 sudo apt install apache2
 sudo apt install php libapache2-mod-php php-mysql
-sudo cp ./info.php /var/www/html/info.php
-sudo apt install stress-ng
 git clone https://github.com/opsengine/cpulimit.git
+sudo cp ./info.php /var/www/html/info.php
+cd stress-ng
+sudo apt install libaio-dev libapparmor-dev libattr1-dev libbsd-dev libcap-dev libgcrypt11-dev libipsec-mb-dev libkeyutils-dev libsctp-dev zlib1g-dev
+sudo apt install make gcc
+make
+cd ..
 cd cpulimit
 make
 sudo cp src/cpulimit /usr/bin/cpulimit
