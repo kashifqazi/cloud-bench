2494e54395c5ca5ac5312010f3713e504ac7ddc7
kashifqazi <kmq2@njit.edu>
2019-08-05 10:56:58 -0400

    Auto yes to installer

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
-sudo apt update
-sudo apt install apache2
-sudo apt install php libapache2-mod-php php-mysql
+yes | sudo apt update
+yes | sudo apt install apache2
+yes | sudo apt install php libapache2-mod-php php-mysql
 git clone https://github.com/opsengine/cpulimit.git
 sudo cp ./*.php /var/www/html/
 cd stress-ng
-sudo apt install libaio-dev libapparmor-dev libattr1-dev libbsd-dev libcap-dev libgcrypt11-dev libipsec-mb-dev libkeyutils-dev libsctp-dev zlib1g-dev
-sudo apt install make gcc
+yes | sudo apt install libaio-dev libapparmor-dev libattr1-dev libbsd-dev libcap-dev libgcrypt11-dev libipsec-mb-dev libkeyutils-dev libsctp-dev zlib1g-dev
+yes | sudo apt install make gcc
 make
 cd ..
 cd atop
-sudo apt install libncurses5-dev
+yes | sudo apt install libncurses5-dev
 make
 cd ..
 cd cpulimit
