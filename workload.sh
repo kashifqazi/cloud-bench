1bc906906c3b5cdeaf55ff91b3a29298200b3b6a
kashifqazi <kmq2@njit.edu>
2019-08-08 13:44:01 -0400

    Improving workload.sh

--- workload.sh
+++ workload.sh
@@ -7,23 +7,21 @@ echo "Host:$1" > config
 echo "Mem:$2" >> config
 echo "Scale:$3" >> config
 
-atop/atopsar -c -m 5 15000 > logs &
+atop/atopsar -c -m 5 150000 > logs &
 while IFS= read -r line
 do
-  values=$(echo $line | tr "," "\n")
-
-  cpu=${values[1]}
-  me=${values[2]}
+  cpu=$(echo $line | cut -d"," -f2)
+  me=$(echo $line | cut -d"," -f3)
 
   #cpu=$(bc -l <<<"${cp}*100")
   #mem=$(bc -l <<<"${me}*100")
-  echo "$cp"
+  echo "$cpu"
   echo "$me"
-  count=`expr $count + 1`
-  if [ $count -ge 800 ]
-  then
-    break
-  fi
+  #count=`expr $count + 1`
+  #if [ $count -ge 2 ]
+  #then
+  #  break
+  #fi
   memory=$(bc -l <<<"${me}*${mem}*1024*1024*1024/100")
   unit="b"
   eval "sudo cpulimit -i -l $cpu stress-ng/stress-ng --vm 1 --vm-bytes $memory$unit -t $scale --times --metrics-brief --perf --log-brief >> stats"
