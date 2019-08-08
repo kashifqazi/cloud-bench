b748610be17973fcb8a26074f26b81d736408ee9
kashifqazi <kmq2@njit.edu>
2019-08-08 13:11:04 -0400

    Adding Alibaba Traces

--- workload.sh
+++ workload.sh
@@ -10,17 +10,23 @@ echo "Scale:$3" >> config
 atop/atopsar -c -m 5 15000 > logs &
 while IFS= read -r line
 do
-  result=$(bc -l <<<"${line}*100")
-  echo "$result"
+  values=$(echo $line | tr "," "\n")
 
+  cpu=${values[1]}
+  me=${values[2]}
+
+  #cpu=$(bc -l <<<"${cp}*100")
+  #mem=$(bc -l <<<"${me}*100")
+  echo "$cp"
+  echo "$me"
   count=`expr $count + 1`
   if [ $count -ge 800 ]
   then
     break
   fi
-  memory=$(bc -l <<<"${result}*${mem}*1024*1024*1024/100")
+  memory=$(bc -l <<<"${me}*${mem}*1024*1024*1024/100")
   unit="b"
-  eval "sudo cpulimit -i -l $result stress-ng/stress-ng --vm 1 --vm-bytes $memory$unit -t $scale --times --metrics-brief --perf --log-brief >> stats"
+  eval "sudo cpulimit -i -l $cpu stress-ng/stress-ng --vm 1 --vm-bytes $memory$unit -t $scale --times --metrics-brief --perf --log-brief >> stats"
   echo "\n" >> stats
   #cat stats >> statscomplete
 done < "$input"
